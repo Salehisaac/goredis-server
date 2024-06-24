573d23c637ee3d68e8ac2c73693eb1e540b3c436
saleh <salehisaac200281@gmail.com>
2024-06-24 04:32:26 +0330

    adding tests

--- client/client_test.go
+++ client/client_test.go
@@ -1,12 +1,30 @@
 package client
 
-import(
-	"testing"
-	"log"
-	"time"
+import (
 	"context"
 	"fmt"
+	"log"
+	"sync"
+	"testing"
+	"time"
 )
+
+
+func TestNewClient1(t *testing.T){
+	client,err := New("localhost:5001")
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(time.Second)
+		if err := client.Set(context.Background(), "foo", "1"); err != nil {
+			log.Fatal(err)
+		}
+		val ,err := client.Get(context.Background(), "foo") 
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(val)
+}
 
 
 func TestNewClient(t *testing.T){
@@ -28,4 +46,35 @@ func TestNewClient(t *testing.T){
 		}
 		fmt.Println("GET => " , val)
 	}
+}
+
+func TestNewClients(t *testing.T){
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func(it int){
+			client,err := New("localhost:5001")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer client.Close()
+			key := fmt.Sprintf("client_foo_%d", it)
+			value := fmt.Sprintf("client_bar_%d", it)
+			if err := client.Set(context.Background(), key, value); err != nil {
+				log.Fatal(err)
+			}
+			val ,err := client.Get(context.Background(), key) 
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("client %d got this value back => %s\n", it, val)
+			wg.Done()
+		}(i)
+
+	}
+	wg.Wait()
+}
+
+func (c *Client) Close()error{
+	return c.conn.Close()
 }
